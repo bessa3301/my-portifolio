7d8ddacee4a188f7d5dfdfb19a38b1d56e98a3a9
kvass33 <bessa.3301@gmail.com>
2022-05-01 23:10:55 -0300

    improve tech ss index page

--- resources/views/tech-stack/index.blade.php
+++ resources/views/tech-stack/index.blade.php
@@ -7,15 +7,14 @@
                     @foreach ($stacks as $stack)
                         <div class="group relative">
                             <div
-                                class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                                <img src="{{ asset('storage/' . $stack->file_path) }}"
-                                    alt="Front of men&#039;s Basic Tee in black."
+                                class="w-full min-h-72 bg-gray-200 aspect-w-1 aspect-h-1 rounded-lg overflow-hidden group-hover:opacity-75 lg:h-72 lg:aspect-none">
+                                <img src="{{ asset('storage/' . $stack->file_path) }}" alt="{{ $stack->name }}"
                                     class="w-full h-full object-center object-cover lg:w-full lg:h-full">
                             </div>
                             <div class="mt-4 flex justify-between">
                                 <div>
                                     <h3 class="text-sm text-gray-700">
-                                        <a href="#">
+                                        <a href="/tech-stack/{{ $stack->id }}">
                                             <span aria-hidden="true" class="absolute inset-0"></span>
                                             {{ $stack->name }}
                                         </a>
